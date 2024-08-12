61b6bde8f36523f09874c3f0a7772279a4d464b8
Kartikeya <kudlu2004@gmail.com>
2024-08-12 23:59:29 +0530

    Added cron job

--- app/api/populate-news-data-on-map/route.ts
+++ app/api/populate-news-data-on-map/route.ts
@@ -1,5 +1,4 @@
-import { createSupabaseServer } from "@/lib/supabase/server";
-import cron from "node-cron";
+import supabaseAdmin from "@/lib/supabase/admin";
 
 interface Article {
   title: string;
@@ -22,132 +21,155 @@ interface NewsData {
   };
 }
 
-async function fetchAndStoreNewsData() {
-  const supabase = createSupabaseServer();
+async function GET(req: Request) {
+  const supabase = supabaseAdmin();
   const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
 
   if (!apiKey) {
-    console.error("News API key not found on /populate-news-data-on-map");
+    console.error("News API key not found on /fetch-news-data");
     return;
   }
 
-  const baseUrl = "https://www.newsapi.ai/api/v1/article/getArticles";
+  const baseUrl = "https://newsapi.ai/api/v1/article/getArticles";
 
-  const queryObject: any = {
-    $query: {
-      $and: [
-        {
-          $or: [
-            {
-              categoryUri:
-                "dmoz/Society/Philanthropy/Disaster_Relief_and_Recovery",
-            },
-            {
-              categoryUri:
-                "dmoz/Science/Earth_Sciences/Natural_Disasters_and_Hazards",
-            },
-          ],
-        },
-      ],
+  const requestBody = {
+    query: {
+      $query: {
+        $or: [
+          {
+            categoryUri:
+              "dmoz/Science/Earth_Sciences/Natural_Disasters_and_Hazards",
+          },
+          {
+            categoryUri:
+              "dmoz/Society/Philanthropy/Disaster_Relief_and_Recovery",
+          },
+        ],
+      },
+      $filter: {
+        forceMaxDataTimeWindow: "31",
+      },
     },
+    resultType: "articles",
+    articlesSortBy: "date",
+    apiKey: apiKey,
+    includeArticleLocation: true,
+    includeLocationGeoLocation: true,
+    includeLocationPopulation: true,
+    includeLocationGeoNamesId: true,
+    articlesPage: 1, // Corrected property name
+    articlesCount: 100,
   };
 
-  const params = new URLSearchParams({
-    query: JSON.stringify(queryObject),
-    resultType: "articles",
-    includeArticleSocialScore: "true",
-    includeArticleConcepts: "true",
-    includeArticleCategories: "true",
-    includeArticleLocation: "true",
-    includeArticleImage: "true",
-    includeArticleVideos: "true",
-    includeArticleLinks: "true",
-    includeArticleExtractedDates: "true",
-    includeArticleOriginalArticle: "true",
-    includeConceptImage: "true",
-    includeConceptDescription: "true",
-    includeConceptSynonyms: "true",
-    includeConceptTrendingScore: "true",
-    includeLocationGeoLocation: "true",
-    includeLocationPopulation: "true",
-    includeLocationGeoNamesId: "true",
-    apiKey: apiKey,
-  });
-
-  const url = `${baseUrl}?${params.toString()}`;
-
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `HTTP error! Status: ${response.status}, Message: ${errorText}`
-      );
-    }
-    const result: NewsData = await response.json();
+    let hasMoreArticles = true;
 
-    for (const article of result.articles.results) {
-      const { eventUri, url: readUrl, dateTime, location } = article;
-      const { lat, long, population } = location;
+    while (hasMoreArticles) {
+      const response = await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
 
-      if (
-        lat != 22 &&
-        long != 79 &&
-        eventUri &&
-        readUrl &&
-        dateTime &&
-        population
-      ) {
-        // Check if the URL already exists in the database
-        const { data: existingData, error: fetchError } = await supabase
-          .from("newsIncidentLocations")
-          .select("id")
-          .eq("read_url", readUrl)
-          .limit(1)
-          .single();
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+          `HTTP error! Status: ${response.status}, Message: ${errorText}`
+        );
+      }
 
-        if (fetchError) {
-          console.error(
-            "Error fetching data from Supabase:",
-            fetchError.message
-          );
-          continue;
-        }
+      const result = await response.json();
 
-        if (!existingData) {
-          const { error: insertError } = await supabase
-            .from("newsIncidentLocations")
-            .insert([
-              {
-                lat,
-                long,
-                read_url: readUrl,
-                updated_at: dateTime,
-                population,
-              },
-            ]);
+      console.log(result.articles);
 
-          console.log("Inserted new data");
+      if (result.articles.results && result.articles.results.length > 0) {
+        console.log(`Fetched ${result.articles.length} articles`);
+        let count = 0,
+          total = 0;
+        for (const article of result.articles.results) {
+          console.log(article.location);
+          total++;
 
-          if (insertError) {
-            console.error(
-              "Error inserting data into Supabase:",
-              insertError.message
-            );
+          if (article.location) {
+            count++;
+            const { eventUri, url: readUrl, dateTime, location } = article;
+            const { lat, long, population } = location;
+
+            // console.log(article.results[0]);
+
+            if (
+              lat != 22 &&
+              long != 79 &&
+              eventUri &&
+              readUrl &&
+              dateTime &&
+              population
+            ) {
+              // Check if the URL already exists in the database
+              const { data: existingData, error: fetchError } = await supabase
+                .from("newsIncidentLocations")
+                .select("id")
+                .eq("read_url", readUrl);
+
+              if (fetchError) {
+                console.error(
+                  "Error fetching data from Supabase:",
+                  fetchError.message
+                );
+                continue;
+              }
+
+              if (!existingData || existingData.length === 0) {
+                const { error: insertError } = await supabase
+                  .from("newsIncidentLocations")
+                  .insert([
+                    {
+                      lat,
+                      long,
+                      read_url: readUrl,
+                      updated_at: dateTime,
+                      population,
+                    },
+                  ]);
+
+                console.log("Inserted new data");
+
+                if (insertError) {
+                  console.error(
+                    "Error inserting data into Supabase:",
+                    insertError.message
+                  );
+                }
+              } else {
+                console.log(
+                  "Data already exists in the database, skipping insertion"
+                );
+              }
+            }
           }
-        } else {
-          console.log(
-            "Data already exists in the database, skipping insertion"
-          );
         }
+
+        console.log(`Inserted ${count}/${total} new records`);
+
+        requestBody.articlesPage += 1;
+      } else {
+        hasMoreArticles = false;
+        console.log("No more articles found");
       }
     }
+
+    return Response.json({
+      success: true,
+      message: "News data fetched successfully",
+    });
   } catch (error) {
     console.error("Error fetching news data:", (error as Error).message);
+
+    return Response.json({
+      success: false,
+      message: "Error fetching news data",
+    });
   }
 }
-
-// Schedule the task to run every second
-cron.schedule("* * * * * *", fetchAndStoreNewsData);
-
-console.log("Cron job is running to fetch and store news data every second.");
